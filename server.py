227a222a1230cb4d7183528d7e17365636f6642b
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-10-07 21:55:43 +0200

    Completed exercise

--- server.py
+++ server.py
@@ -21,17 +21,16 @@ def too_low():
 
 def too_high():
     return f'<h1 style="color:Purple;">To high, try again</h1>\n' \
-            f'<img src=https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif>'
+           f'<img src=https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif>'
 
 
 def equal():
-    return '<h1 style="color:Green;">You find me!</h1>\n'\
-            f'<img src=https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif>'
+    return '<h1 style="color:Green;">You find me!</h1>\n' \
+           f'<img src=https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif>'
 
 
 @app.route('/')
 def main():
-
     return '<h1>Guess a number between 0 and 9</h1>' \
            '<input type="number">' \
            '<img src=https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif>'
